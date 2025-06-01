5fef36d08c405dca044efa1c4394f62a1425de4c
Carsten Lee <carstenlee845@gmail.com>
2025-06-01 17:39:47 -0400

    Fixed the heads up display and added images to the musicartists quiz

--- CPTCarsten.java
+++ CPTCarsten.java
@@ -30,33 +30,33 @@ public class CPTCarsten{
 			con.println("\nEnter name of quiz:"); 
 			strUserQuiz = con.readLine(); 
 			
-			// Step 1: Count number of questions (6 lines per question)
+			// Step 1: Count number of questions (7 lines per question)
 			TextInputFile countQuestions = new TextInputFile(strUserQuiz);
 			int intLineCount = 0;
 			while (!countQuestions.eof()) {
 				countQuestions.readLine();
 				intLineCount++;
 			}
-			int intQuestionCount = intLineCount / 6;
+			int intQuestionCount = intLineCount / 7;
 
-			// Step 2: Create 2D array [# of questions][7 columns]
-			String[][] strQuiz = new String[intQuestionCount][7];
+			// Step 2: Create 2D array [# of questions][8 columns] (one extra for the image) 
+			String[][] strQuiz = new String[intQuestionCount][8];
 
 			// Step 3: Load data into array
 			TextInputFile quizFile = new TextInputFile(strUserQuiz);
 			for (int i = 0; i < intQuestionCount; i++) {
-				for (int j = 0; j < 6; j++) {
+				for (int j = 0; j < 7; j++) {
 					strQuiz[i][j] = quizFile.readLine();
 				}
-				// Column 6: Random number to shuffle
-				strQuiz[i][6] = String.valueOf((int)(Math.random() * 100 + 1));
+				// Column 7: Random number to shuffle
+				strQuiz[i][7] = String.valueOf((int)(Math.random() * 100 + 1));
 			}
 
-			// Step 4: Bubble sort based on column 6
+			// Step 4: Bubble sort based on column 7
 			for (int i = 0; i < strQuiz.length - 1; i++) {
 				for (int j = 0; j < strQuiz.length - i - 1; j++) {
-					int num1 = Integer.parseInt(strQuiz[j][6]);
-					int num2 = Integer.parseInt(strQuiz[j + 1][6]);
+					int num1 = Integer.parseInt(strQuiz[j][7]);
+					int num2 = Integer.parseInt(strQuiz[j + 1][7]);
 					if (num1 > num2) {
 						// Swap rows
 						String[] temp = strQuiz[j];
@@ -68,32 +68,38 @@ public class CPTCarsten{
 				
 			// Step 5: Ask questions and calculate score
 			int intScore = 0;
+			int intPercentage = 0; 
 			for (int i = 0; i < strQuiz.length; i++) {
-				int intPercentage = (intScore/strQuiz.length)*100;
-				con.println("Name: "+strName+"     Quiz: "+strUserQuiz+"     Score: "+intPercentage+"\n");
-				
-				con.clear();
+				con.clear(); 
+
+				con.println("Name: "+strName+"     Quiz: "+strUserQuiz+"     Score: "+intPercentage+"%\n");
 				
 				con.println("Question " + (i + 1) + ": " + strQuiz[i][0]);
-				con.println("A) " + strQuiz[i][1]);
-				con.println("B) " + strQuiz[i][2]);
-				con.println("C) " + strQuiz[i][3]);
-				con.println("D) " + strQuiz[i][4]);
+				BufferedImage imgQuestion = con.loadImage(strQuiz[i][1]); 
+				con.drawImage(imgQuestion,0,80); 
+				con.repaint();
+				con.println("\n\n\n\n\n\n\n\n\nA) " + strQuiz[i][2]);
+				con.println("B) " + strQuiz[i][3]);
+				con.println("C) " + strQuiz[i][4]);
+				con.println("D) " + strQuiz[i][5]);
 				con.print("Your answer (A/B/C/D): ");
 				String answer = con.readLine();
 
-				if (answer.equalsIgnoreCase(strQuiz[i][5])) {
+				if (answer.equalsIgnoreCase(strQuiz[i][6])) {
 					con.println("Correct!");
 					intScore++;
 				} else {
-					con.println("Incorrect. Correct answer: " + strQuiz[i][5]);
+					con.println("Incorrect. Correct answer: " + strQuiz[i][6]);
 				}
 				con.println("Press any key to continue...");
+				intPercentage = (int)(((double)intScore / (i+1)) * 100);
 				con.getChar();
 			}
-
+			con.setDrawColor(Color.BLACK); 
+			con.fillRect(0,80,200, 200);
 			con.clear();
 			con.println("Quiz complete!");
+			con.println("You scored "+intPercentage+"%"); 
 			
 		}
 		
